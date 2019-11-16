b027f21335c840f66a60f359df2e8bf59d2421c2
Thomas Acland <46223561+tacland@users.noreply.github.com>
2019-11-16 11:40:37 +0100

    complete task sum-all.ts

--- Arrays/sum-all.ts
+++ Arrays/sum-all.ts
@@ -1,22 +1,20 @@
 // -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
 // -  Log the sum of the elements in `ai` to the console
 
-let ai = [3, 4, 5, 6, 7];
+let ai = [];
 
-let myNumber = ai[4];
+function findSum(myNumbers) {
 
-function findSum(c: number) {
+  let result= 0;
 
-    let result = ai[0];
+      for(let i = 0; i < myNumbers.length; i++) {
+        result = result + myNumbers[i];
+      }
+      return result;    
+}
 
-    for(let i = 0; i <= ai[4]; i++) {
-      result = result + i;
-      return result;
-    }
+ai = [3, 4, 5, 6, 7];
 
-    
-    return result;
-  }
-  
-findSum(ai[4])
-console.log(findSum(ai[4]));
+console.log(findSum(ai));
+
+export{}
