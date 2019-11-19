002e3a78665d0b3adaf439145158ca2ce0efe3e1
Thomas Acland <46223561+tacland@users.noreply.github.com>
2019-11-19 17:13:24 +0100

    update reverse.ts file

---
+++ Arrays/reverse.ts
@@ -0,0 +1,20 @@
+// -  Create a variable named `numList`
+//    with the following content: `[3, 4, 5, 6, 7]`
+// -  Reverse the order of the elements in `numList`
+// 	   -  do it with the built in method
+//	   -  do it with creating a new temp array and a loop
+// -  Print the elements of the reversed `numList`
+
+// 	   -  do it with the built in method
+
+let numList = [3, 4, 5, 6, 7];
+
+/*
+numList.reverse();
+
+console.log(numList); 
+*/
+
+//	   -  do it with creating a new temp array and a loop
+
+const reverse = 
