654e59a3dc5e9195899b8526e6d2be7e00e82511
Thomas Acland <46223561+tacland@users.noreply.github.com>
2019-11-23 11:18:39 +0100

    upload he-will-never.ts

---
+++ data-structures/he-will-never.ts
@@ -0,0 +1,30 @@
+'use strict';
+// Things are a little bit messed up
+// Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
+// Assemble the fragments into the out variable
+
+let out: string = '';
+let notSoCrypticMessage: number[] = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
+
+let hashmap = {
+  7: 'run around and desert you',
+  50: 'tell a lie and hurt you ',
+  49: 'make you cry, ',
+  2: 'let you down',
+  12: 'give you up, ',
+  1: 'Never gonna ',
+  11: '\n',
+  3: 'say goodbye '
+};
+
+function decoder (a:string, b:number[], c:object){
+    for(let i = 0; i < 16; i++){
+        a = a.concat(c[b[i]])
+    }
+    return a
+}
+
+out = decoder(out, notSoCrypticMessage, hashmap);
+
+
+console.log(out)
