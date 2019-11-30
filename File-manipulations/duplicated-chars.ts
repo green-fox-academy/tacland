5503611c88b5b660c349783626cb349dbc4ebabb
Thomas Acland <46223561+tacland@users.noreply.github.com>
2019-11-30 20:24:44 +0100

    final version of duplicated-chars

--- File-manipulations/duplicated-chars.ts
+++ File-manipulations/duplicated-chars.ts
@@ -9,9 +9,10 @@ for(let i = 0; i < splitByLine.length - 1; i++){
     for(let j = 0; j < splitByLine[i].length; j++){
 
         if( j % 2 === 0){
-        newArray = newArray.concat(splitByLine[i].charAt(j))
-        }        
+            newArray = newArray.concat(splitByLine[i].charAt(j))
+        }    
     }
+    newArray = newArray.concat('\n')
 }
 
 console.log(newArray)
