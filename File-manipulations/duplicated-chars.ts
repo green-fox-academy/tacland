ece6b5c728a4652e77c480c2bf1ed30b0043090a
Thomas Acland <46223561+tacland@users.noreply.github.com>
2019-11-30 20:20:39 +0100

    gitignore update, duplicated-chars optimisation

--- File-manipulations/duplicated-chars.ts
+++ File-manipulations/duplicated-chars.ts
@@ -4,11 +4,6 @@ let originFile:string = fs.readFileSync('./assets/duplicated-chars.txt', 'utf-8'
 let splitByLine = originFile.split('\n')
 let newArray:string = ''
 
-// console.log(splitByLine)
-
-
-// console.log(newArray = newArray.concat(splitByLine[0].charAt(0)))
-
 for(let i = 0; i < splitByLine.length - 1; i++){
 
     for(let j = 0; j < splitByLine[i].length; j++){
@@ -20,29 +15,5 @@ for(let i = 0; i < splitByLine.length - 1; i++){
 }
 
 console.log(newArray)
-
-// console.log(newArray)
-
-// for(let i = 0; i < splitByLine.length - 1; i++)
-// if ( splitByLine[i].charAt(i)    ){
-
-// }
-
-//let splitBySpace = splitByLine.split(' '),
-
-//soronkent split
-//elemekre bontas
-//kivenni masodik elemeket
-// paros-e => 6%2 === 0
-
-
-
-// for(let i = 0; i < originFile.length - 1; i+2 ) {
-//     originFile.splice(i, 1)
-// }
-
-// console.log(result)
-
-// console.log(result)
 
 export{}
