36a997af6040dbb9698f2f27573eff94d9a7e366
Thomas Acland <46223561+tacland@users.noreply.github.com>
2019-12-02 19:25:09 +0100

    clear reversed-lines

--- File-manipulations/reversed-lines.ts
+++ File-manipulations/reversed-lines.ts
@@ -1,11 +1,4 @@
 // Create a method that decrypts reversed-lines.txt
-// var sentence = 'The quick brown fox jumps over the lazy dog.';
-
-// var index = 4;
-
-// console.log('The character at index ' + index + ' is ' + sentence.charAt(index));
-// expected output: "The character at index 4 is q"
-
 
 const fs = require('fs');
 
