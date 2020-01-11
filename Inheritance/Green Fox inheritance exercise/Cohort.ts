12c63fcffa889c51eef187392cdb1a8d65265abb
Thomas Acland <46223561+tacland@users.noreply.github.com>
2020-01-11 15:49:45 +0100

    upload Cohort.ts + complete task Green Fox inheritance exercise

--- Inheritance/Green Fox inheritance exercise/Cohort.ts
+++ Inheritance/Green Fox inheritance exercise/Cohort.ts
@@ -1,3 +1,27 @@
+import { Person } from "./Person"
+import { Student} from "./Student"
+import { Mentor} from "./Mentor"
+
 export class Cohort {
-    
+    name: string;
+    students: Student[]; //list of students
+    mentors: Mentor[]; //list of Mentors
+
+    constructor (name: string) {
+        this.name = name;
+        this.students = [];
+        this.mentors = [];
+    }
+
+    addStudent(newStudent){
+        this.students.push(newStudent);
+    }
+
+    addMentor(newMentor){
+        this.mentors.push(newMentor);
+    }
+
+    info(){
+        console.log('\'The ' + this.name + ' cohort has ' + Object.keys(this.students).length + ' students and ' + Object.keys(this.mentors).length + ' mentors.\'');
+    }
 }
